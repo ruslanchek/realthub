18b91a51f093a882bb0bf0562fcc640bd3f162c3
ruslanchek <rshashkov@icloud.com>
2019-08-21 18:50:49 +0300

    Update

--- api/fixtures/properties.ts
+++ api/fixtures/properties.ts
@@ -2,9 +2,9 @@ import { IProperty } from '../../meta/interfaces';
 import { generate } from '../lib/api';
 import faker from 'faker';
 
-export const properties = generate<IProperty>(10, () => {
+export const properties = generate<IProperty>(10, index => {
 	return {
-		id: faker.random.uuid(),
+		id: index.toString(),
 		title: faker.address.streetAddress(),
 	};
 });
