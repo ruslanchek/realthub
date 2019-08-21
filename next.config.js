bc8f31e262a54d0f648bc7970a39c1bac726ed71
ruslanchek <rshashkov@icloud.com>
2019-08-21 17:47:28 +0300

    Upd

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
-module.exports = {
-	target: 'serverless',
-	env: {
+const env = process.env.NODE_ENV || 'development';
+const vars = {
+	development: {
 		API_URL: 'http://localhost:3111/api',
 		GOOGLE_MAPS_API_KEY: 'AIzaSyCx2_EqHzYNxnEf0kURZFbjxYbuF8jqbK0',
 	},
+	staging: {
+		API_URL: 'https://realthub.com/api',
+		GOOGLE_MAPS_API_KEY: 'AIzaSyCx2_EqHzYNxnEf0kURZFbjxYbuF8jqbK0',
+	},
+	production: {
+		API_URL: 'https://realthub.com/api',
+		GOOGLE_MAPS_API_KEY: 'AIzaSyCx2_EqHzYNxnEf0kURZFbjxYbuF8jqbK0',
+	},
+}[env];
+
+module.exports = {
+	target: 'serverless',
+	env: vars,
 };
